57a06a37594ea229d82d9c696002438372b4cb70
“kkiirr201314” <qq255073@gmail.com>
2021-06-24 16:01:24 +0800

    c10-26

--- ch10.kt
+++ ch10.kt
@@ -18,4 +18,44 @@ fun main() {
 
     }
     println(uniquePatrons)
+    var orderCount = 0
+    while (orderCount <= 9){
+        placeOrder(uniquePatrons.shuffled().first(),
+            menuList.shuffled().first())
+        orderCount++
+    }
+}
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiouAEIOU]")){
+        when (it.value){
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            "A" -> "4"
+            "E" -> "3"
+            "I" -> "1"
+            "O" -> "0"
+            "U" -> "|_|"
+            else -> it.value
+        }
+    }
+private  fun placeOrder(patronName: String, menuData: String){
+    val indexOfAppstrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfAppstrophe)
+    println("$patronName 對 $tavernMaster 說了他們訂的酒水。 ")
+    val (type,name,price) = menuData.split(',')
+    val message = "$patronName 買了一杯 $name ($type) 花了 $price."
+    println(message)
+    //  performPurchase(price.toDouble())
+    //  val phrase = "Ah, dlicious $name!"
+    //  println("Madrigal 驚呼道: ${toDragonSpeak(phrase)}")
+    val phrase = if (name == "Dragon's Breath"){
+        "$patronName 驚呼道: ${toDragonSpeak("Ah, dlicious $name!")}"
+    } else {
+        "$patronName 說道: 感謝 $name ."
+    }
+    println(phrase)
+    println("")
 }
